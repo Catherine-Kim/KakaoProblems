d2fb6cbc45e6a3a9ca7cf9119239da67c9c774f9
user-PC\user <woojong610@gmail.com>
2017-08-27 15:58:54 +0900

    <하샤드수> 모범 답안(2개)

--- src/level2/HarshadNumber.java
+++ src/level2/HarshadNumber.java
@@ -10,6 +10,31 @@ public class HarshadNumber{
 		
 		return num%sum == 0;
 	}
+	
+	
+	/* ixymori 님 풀이
+	public boolean isHarshad(int num){
+        char[] charArr = String.valueOf(num).toCharArray();
+    int divisor = 0;
+
+    for (char c : charArr) {
+        divisor += (int) c - 48;
+    }
+    */
+	
+	/* 또롱 님 풀이
+	public boolean isHarshad(int num) {
+		int sum = 0, n = num;
+		while (n != 0) {
+			sum += n % 10;
+			n /= 10;
+		}
+		if (num % sum == 0)
+			return true;
+		else
+			return false;
+	}
+	*/
   
        // 아래는 테스트로 출력해 보기 위한 코드입니다.
 	public static void  main(String[] args){
