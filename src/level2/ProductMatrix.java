bff91474e3afa49404fa7ca7ba8f52ae557d6ecb
user-PC\user <woojong610@gmail.com>
2017-08-27 15:46:12 +0900

    <행렬의 곱셈> 문제 해결

---
+++ src/level2/ProductMatrix.java
@@ -0,0 +1,46 @@
+package level2;
+
+class ProductMatrix {
+	public int[][] productMatrix(int[][] A, int[][] B) {
+		int answerRowSize = A.length;
+		int answerColumnSize = B[0].length;
+		int middleSize = A[0].length;
+		int sum = 0;
+		int[][] answer = new int[answerRowSize][answerColumnSize];
+
+		for(int ar = 0; ar < answerRowSize; ar++){
+			for(int ac = 0; ac < answerColumnSize; ac++){
+				sum = 0;
+				for(int ms = 0; ms < middleSize; ms++){
+					//System.out.println("GOT IN THE DEEPEST FOR");
+					sum += A[ar][ms] * B[ms][ac];
+					//System.out.println(A[ar][ms] + " * " + B[ms][ac] + " = " + A[ar][ms] * B[ms][ac]);
+					//System.out.println("sum : " + sum);
+				}
+				answer[ar][ac] = sum;
+			}
+		}
+		
+		return answer;
+	}
+
+	public static void main(String[] args) {
+		ProductMatrix c = new ProductMatrix();
+		int[][] a = { { 1, 2 }, { 2, 3 } };
+		int[][] b = { { 3, 4 }, { 5, 6 } };
+		
+		// 아래는 테스트로 출력해 보기 위한 코드입니다.
+	    int[][] ret = c.productMatrix(a, b);
+	    
+	    System.out.println("(");
+	    for(int i = 0; i < ret.length; i++){
+	    	System.out.print("(");
+	    	for(int j = 0; j < ret.length; j++){
+	    		System.out.print(ret[i][j]);
+	    		System.out.print(", ");
+	    	}
+	    	System.out.println("), ");
+	    }
+	    System.out.println(")");
+	}
+}
