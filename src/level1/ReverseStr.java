cf3e9578c6cafcc31f5c19e56710379f2f456d46
user-PC\user <woojong610@gmail.com>
2017-08-27 10:02:21 +0900

    ReverseStr 모범 답안

--- src/level1/ReverseStr.java
+++ src/level1/ReverseStr.java
@@ -64,6 +64,16 @@ public class ReverseStr {
 		
 		return resultSB.toString();
 	}
+	
+	/* Sunjin Lee 님 풀이
+	
+	public String reverseStr(String str){
+		char[] sol = str.toCharArray();
+    	Arrays.sort(sol);
+    	return new StringBuilder(new String(sol)).reverse().toString();
+    }
+	
+	 */
 
 	// 아래는 테스트로 출력해 보기 위한 코드입니다.
 	public static void main(String[] args) {
