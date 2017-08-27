1560ca90533abd8b609a7adad9ed236a9e9393ad
user-PC\user <woojong610@gmail.com>
2017-08-27 10:14:11 +0900

    <나누어 떨어지는 숫자 배열> 모범 답안

--- src/level1/Divisible.java
+++ src/level1/Divisible.java
@@ -20,6 +20,17 @@ class Divisible {
 			
 		return ret;
 	}
+	
+	/* roeuihyun 님 풀이
+	public int[] divisible(int[] array, int divisor) {
+        //ret에 array에 포함된 정수중, divisor로 나누어 떨어지는 숫자를 순서대로 넣으세요.
+        return Arrays.stream(array).filter(factor -> factor % divisor == 0).toArray();
+    }
+	*/
+	
+		
+	 
+	
 	// 아래는 테스트로 출력해 보기 위한 코드입니다.
 	public static void main(String[] args) {
 		Divisible div = new Divisible();
