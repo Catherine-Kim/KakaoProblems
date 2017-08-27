496b7f4b27be0b5bbd26bc7eaa1b46aeab9ca668
user-PC\user <woojong610@gmail.com>
2017-08-27 16:54:21 +0900

    <소수 찾기> 모범 답안

--- src/level2/NumOfPrime.java
+++ src/level2/NumOfPrime.java
@@ -21,6 +21,23 @@ class NumOfPrime {
 		
 		return result;
 	}
+	
+	/* 홍희경 님 풀이
+	int numberOfPrime(int n) {
+		int result = 0;
+		for (int i = 2; i <= n; i++) {
+			for (int j = 2; j <= i; j++) {
+				if (j == i) {
+					++result;
+				} else if (i % j == 0) {
+					break;
+				}
+			}
+		}
+
+		return result;
+	}
+	*/
 
 	public static void main(String[] args) {
 		NumOfPrime prime = new NumOfPrime();
