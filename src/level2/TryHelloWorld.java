a0760e73a04591e5a28a12ed5a4c86e709465929
user-PC\user <woojong610@gmail.com>
2017-08-27 13:22:20 +0900

    <최솟값 만들기> 문제 해결

--- src/level2/TryHelloWorld.java
+++ src/level2/TryHelloWorld.java
@@ -1,5 +1,28 @@
 package level2;
 
-public class TryHelloWorld {
+import java.util.Arrays;
 
+class TryHelloWorld {
+    public int getMinSum(int []A, int []B) {
+        int answer = 0;
+        
+        Arrays.sort(A);
+        Arrays.sort(B);
+       
+        int j = A.length - 1;
+        for(int i = 0; i < A.length; i++){
+        	answer += A[i] * B[j];
+        	j--;
+        }
+        
+        return answer;
+    }
+
+    
+    public static void main(String[] args) {
+        TryHelloWorld test = new TryHelloWorld();
+        int []A = {1,2};
+        int []B = {3,4};
+        System.out.println(test.getMinSum(A,B));
+    }
 }
