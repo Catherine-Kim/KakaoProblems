7e68acfe67c8e09319970f267825b42d7d84784f
user-PC\user <woojong610@gmail.com>
2017-08-27 15:47:40 +0900

    <행렬의 곱셈> 모범 답안

--- src/level2/ProductMatrix.java
+++ src/level2/ProductMatrix.java
@@ -23,7 +23,24 @@ class ProductMatrix {
 		
 		return answer;
 	}
+	
+	/* JBK 님 풀이
+	public int[][] productMatrix(int[][] A, int[][] B) {
+        int[][] answer = new int[A.length][B[0].length];
+        if(A[0].length == B.length){
+            for (int i = 0; i < answer.length; i++) {
+                for (int j = 0; j < answer[0].length; j++) {
+                    for (int k = 0; k < A[0].length; k++) {
+                        answer[i][j]+=A[i][k]*B[k][j];
+                    }
+                }
 
+            }
+        }
+        return answer;
+    }
+	*/
+	
 	public static void main(String[] args) {
 		ProductMatrix c = new ProductMatrix();
 		int[][] a = { { 1, 2 }, { 2, 3 } };
