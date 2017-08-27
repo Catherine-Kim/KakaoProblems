dea5a27d7839b6303a06b39589237db5bc9f6303
user-PC\user <woojong610@gmail.com>
2017-08-27 16:45:14 +0900

    <정수 내림차순으로 배치하기> 모범 답안

--- src/level2/ReverseInt.java
+++ src/level2/ReverseInt.java
@@ -36,6 +36,14 @@ public class ReverseInt {
 		
 		return result;
 	}
+	
+	/* 덱스또 님 풀이
+	public int reverseInt(int n){
+        res = "";
+        Integer.toString(n).chars().sorted().forEach(c -> res = Character.valueOf((char)c) + res);
+        return Integer.parseInt(res);
+    }
+	*/
   
 	// 아래는 테스트로 출력해 보기 위한 코드입니다.
 	public static void  main(String[] args){
