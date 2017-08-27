5afe5b3baa8a121b3fb421fa65f5bd0589972d32
user-PC\user <woojong610@gmail.com>
2017-08-27 10:26:48 +0900

    <가운데 글자 가져오기> 모범 답안

--- src/level1/StringExercise.java
+++ src/level1/StringExercise.java
@@ -5,6 +5,15 @@ class StringExercise{
     	int len = word.length();
     	return len%2 == 0?word.substring(len/2 - 1).substring(0, 2):word.substring(len/2).substring(0, 1);    
     }
+    
+    /* 강예은 님 풀이
+    String getMiddle(String word){
+        int length = word.length();
+        int index = length / 2;
+          return (length%2==0) ? word.substring(index-1, index+1) : word.substring(index, index+1); 
+      }
+    */
+    
     // 아래는 테스트로 출력해 보기 위한 코드입니다.
     public static void  main(String[] args){
         StringExercise se = new StringExercise();
